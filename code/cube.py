10bf75c033250adcd37c8f7e9fc5f6859363cb85
David W. Hogg <dwhogg@gmail.com>
2012-10-28 16:18:28 -0400

    object skeleton

---
+++ code/cube.py
@@ -0,0 +1,13 @@
+# This file is part of the Magic Cube project.
+# Copyright 2012 David W. Hogg (NYU).
+
+class Cube(object):
+
+    def __init__(N):
+        return None
+
+    def move(layers, direction):
+        return None
+
+    def render(ax):
+        return None
