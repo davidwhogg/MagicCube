8a9d192b235ccdeac951ae64ab3dd5acce112562
stonebig <stonebig34@gmail.com>
2018-08-16 20:47:50 +0200

    python3 compatibility
    https://github.com/davidwhogg/MagicCube/issues/9

--- code/projection.py
+++ code/projection.py
@@ -157,7 +157,7 @@ def project_points(points, q, view, vertical=[0, 1, 0]):
     dpoint_view = np.dot(dpoint, view).reshape(dpoint.shape[:-1] + (1,))
     dproj = -dpoint * v2 / dpoint_view
 
-    trans = range(1, dproj.ndim) + [0]
+    trans =  list(range(1, dproj.ndim)) + [0]
     return np.array([np.dot(dproj, xdir),
                      np.dot(dproj, ydir),
                      -np.dot(dpoint, zdir)]).transpose(trans)
